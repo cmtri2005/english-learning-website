ded505b046afe5b4abb289152cfd1939addc20ff
cmtri2005 <tricaominh2005@gmail.com>
2025-12-22 14:33:07 +0000

    chore: cleanup unused files, update page title to Monolingo

--- backend/migrations/add_content_text_to_groups.sql
+++
@@ -1,6 +0,0 @@
--- Add content_text column to exam_question_groups if it doesn't exist
-ALTER TABLE exam_question_groups 
-ADD COLUMN IF NOT EXISTS content_text TEXT AFTER part_number;
-
--- Verify the change
-DESCRIBE exam_question_groups;
